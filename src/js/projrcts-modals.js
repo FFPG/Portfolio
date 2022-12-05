fa3e473f2178cbf515cbf6cc79b8d5b06963cb12
FFPG <fabian.ffpg.03@gmail.com>
2022-12-05 11:40:08 +0100

    reworked project modals

---
+++ src/js/projrcts-modals.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".ffpg-js-project").forEach(project => {
+    let modal = project.querySelector('dialog'); 
+    project.querySelector('.ffpg-js-modal__open').addEventListener('click', ()=>{
+        modal.showModal(); 
+    });
+    modal.querySelector('.modal-close-btn').addEventListener('click', ()=>{
+        modal.close(); 
+    });
+}); 
