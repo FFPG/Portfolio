73ceae63ca64d8e7f812ee6471c4a85c5f4d6d04
FFPG <fabian.ffpg.03@gmail.com>
2022-12-31 13:54:23 +0100

    new streukture

--- dist/js/visitencard.js
+++ dist/js/visitencard.js
@@ -1,10 +1,14 @@
-let visitencard = document.querySelector('#visitencarte-content'); 
-let slideDown = document.querySelector('#visitencard_back-open'); 
+
 document.querySelector('#visitencardbtnfront').addEventListener('click', ()=>{
+    let visitencard = document.querySelector('#visitencarte-content'); 
+    let slideDown = document.querySelector('#visitencard_back-open'); 
     visitencard.classList.add('turnback');
     slideDown.classList.add('openbackbox'); 
 }); 
 document.querySelector('#visitencardbtnback').addEventListener('click', ()=>{
+    let visitencard = document.querySelector('#visitencarte-content'); 
+    let slideDown = document.querySelector('#visitencard_back-open'); 
     visitencard.classList.remove('turnback');
     slideDown.classList.remove('openbackbox'); 
 })
+
